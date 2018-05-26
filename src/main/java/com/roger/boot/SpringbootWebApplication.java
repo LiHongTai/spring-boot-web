c6ee3a2b7e5f6784b59298fbb02b2587ac0fc4e7
LiHongTai <631396529@qq.com>
2018-05-26 14:21:20 +0800

    过滤器实现

--- src/main/java/com/roger/boot/SpringbootWebApplication.java
+++ src/main/java/com/roger/boot/SpringbootWebApplication.java
@@ -1,10 +1,28 @@
 package com.roger.boot;
 
+import com.roger.boot.common.filter.AccessRecorderFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootWebApplication {
+
+    //将过滤器注册进IOC容器
+    @Bean
+    public FilterRegistrationBean accessRecorderFillerRegister(){
+        FilterRegistrationBean regFilter = new FilterRegistrationBean();
+        //创建并注册AccessRecorderFilter
+        regFilter.setFilter(new AccessRecorderFilter());
+        //添加请求拦截地址，下面设置对所有请求进行拦截
+        regFilter.addUrlPatterns("/");
+        //设置过滤器名字
+        regFilter.setName("accessRecorderFilter");
+        //设置过滤器执行顺序，值越小越先执行（多个过滤器同时存在时）
+        regFilter.setOrder(1);
+        return regFilter;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootWebApplication.class,args);
---
+++ src/main/java/com/roger/boot/common/filter/AccessRecorderFilter.java
@@ -0,0 +1,48 @@
+package com.roger.boot.common.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.temporal.TemporalField;
+
+public class AccessRecorderFilter implements Filter {
+
+    private Logger logger = LoggerFactory.getLogger(AccessRecorderFilter.class);
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        String uri = request.getRequestURI();
+        if(uri.endsWith(".js")
+                || uri.endsWith(".css")
+                || uri.endsWith(".map")
+                || uri.endsWith(".jpg")
+                || uri.endsWith(".png")
+                ){
+            filterChain.doFilter(servletRequest,servletResponse);
+            return;
+        }
+
+        String userAgent = request.getHeader("user-agent");
+        String ip = request.getRemoteAddr();
+        Long startTime = LocalDate.now().toEpochDay();
+        //将请求向后推送，到下一个filter或者最终到Controller执行
+        filterChain.doFilter(servletRequest,servletResponse);
+        Long endTime = LocalDate.now().toEpochDay();
+        logger.info("uri:{},ip:{},accessTime:{},userAgent:{}",uri,ip,(endTime-startTime),userAgent);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
